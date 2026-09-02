6cb0606f2424592ba3cf0568bc52617d9a1876ce
Imogen <me@imogen.tech>
2026-09-02 12:27:36 +0000

    cleanup test script

--- annotation_tests.py
+++ annotation_tests.py
@@ -19,10 +19,11 @@ from types import ModuleType
 from importlib import import_module
 import pkgutil
 import tracemalloc
-from annotationlib import Format, call_annotate_function
+from annotationlib import Format
 import timeit
 from pathlib import Path
 from contextlib import redirect_stdout
+from typing import eval_annotate_as_types
 
 VENV_PATH = Path() / ".venv/Lib/site-packages"
 sys.path.insert(0, str(VENV_PATH))
@@ -96,18 +97,17 @@ for package in PACKAGES:
 print(time)
 """
 
-
-results = [subprocess.run([sys.executable, "-c", code], capture_output=True, text=True).stdout.split() for _ in range(5)]
+procs = [subprocess.run([sys.executable, "-c", code], capture_output=True, text=True) for _ in range(5)]
 import_time, memory, pyc, exec_time = 0, 0, 0, 0
-for result in results:
-    import_time += float(result[0])
-    memory += int(result[1])
-    pyc += int(result[2])
-    exec_time += float(result[3])
-import_time /= len(results)
-memory /= len(results)
-pyc /= len(results)
-exec_time /= len(results)
+for proc in procs:
+    if proc.stderr:
+        print(proc.stderr)
+        raise SystemExit
+    i, m, p, e = (float(x) / len(procs) for x in proc.stdout.split())
+    import_time += i
+    memory += m
+    pyc += p
+    exec_time += e
 
 
 print(
